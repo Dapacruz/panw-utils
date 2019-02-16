2352209ca1e9c73073ac6293d9a0bd93b4b9651c
David Cruz <davidcruz72@gmail.com>
2019-02-16 15:16:16 -0800

    Merge Cookiecutter template

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="David Paul Cruz",
+    author_email='davidcruz72@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Palo Alto Networks Utilities",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='panw_utils',
+    name='panw_utils',
+    packages=find_packages(include=['panw_utils']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/dapacruz/panw_utils',
+    version='0.0.5',
+    zip_safe=False,
+    entry_points={
+        "console_scripts":[
+            'get-panw-api-key=panw_utils.get_panw_api_key:main',
+            'get-panw-firewalls=panw_utils.get_panw_firewalls:main',
+            'get-panw-interfaces=panw_utils.get_panw_interfaces:main',
+        ]
+    },
+)
---
+++ tests/test_panw_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `panw_utils` package."""
+
+
+import unittest
+
+from panw_utils import panw_utils
+
+
+class TestPanw_utils(unittest.TestCase):
+    """Tests for `panw_utils` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
