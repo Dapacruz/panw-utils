829db7438aab475bb5d02f1cc3146cd6ec69eab7
David Cruz <davidcruz72@gmail.com>
2020-09-14 13:52:41 -0700

    Bump version: 0.5.9 → 0.5.10

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dapacruz/panw-utils',
-    version='0.5.9',
+    version='0.5.10',
     zip_safe=False,
     entry_points={
         "console_scripts":[
